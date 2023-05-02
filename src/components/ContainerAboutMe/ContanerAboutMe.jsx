9167eab61b3617bdf979b9cde9784d9ab0988031
camila.pereyra <campereyra23051995@gmail.com>
2023-05-02 10:44:44 -0300

    Corrección parrafo about me

--- src/components/ContainerAboutMe/ContanerAboutMe.jsx
+++ src/components/ContainerAboutMe/ContanerAboutMe.jsx
@@ -5,21 +5,20 @@ const ContanerAboutMe = () => {
     <section id="aboutMe" className="section">
       <h2 className="tittleSection">Sobre mi</h2>
       <p className="aboutMeParagraph">
-        Hola a todxs, espero que se encuentren muy bien! Les quiero contar un
-        resumen de mi trayectoria educativa y profesional. Soy Developer
-        Frontend React JS, realice varios cursos relacionados al desarrollo
-        frontend: desarrollo web, Javascript y React JS. En paralelo, soy
-        estudiante de la carrera Licenciatura en Sistemas en la UNLa. Al
-        comenzar la universidad descubrí que me apasiona el mundo de la
-        tenología, la programación y me encantaría abarcar la mayor cantidad de
-        conocimientos posible. Proximamente comenzaré un curso de backend para
-        poder cumplir uno de mis objetivos que es ser desarolladora Fullstack.
-        Respecto a lo laboral, tengo experiencia freelance desarrollando sitios
-        web. Allí pude poner en practica y potenciar aún más mis conocimientos
-        técnicos y mis habilidades en equipo. Me considero una persona
-        responsable, proactiva, comunicativa, colaborativa, autodidacta y con
-        muchas ganas de seguir aprendiendo y creciendo en este mundo IT que
-        tanto me gusta.
+        Hola a todxs, espero que se encuentren muy bien! Les cuento un poco
+        acerca de mi trayectoria educativa y profesional. Soy Developer Frontend
+        React JS, realice varios cursos relacionados al desarrollo frontend:
+        Desarrollo Web, Javascript y React JS. En paralelo, soy estudiante de la
+        carrera Licenciatura en Sistemas en la UNLa. Al comenzar la universidad
+        descubrí que me apasiona el mundo de la tenología, la programación y me
+        encantaría abarcar la mayor cantidad de conocimientos posible.
+        Proximamente, comenzaré un curso de Backend para poder cumplir uno de
+        mis objetivos que es ser desarolladora Fullstack. Respecto a lo laboral,
+        tengo experiencia freelance desarrollando sitios web. Allí pude poner en
+        práctica y potenciar aún más mis conocimientos técnicos y mis
+        habilidades en equipo. Me considero una persona responsable, proactiva,
+        comunicativa, colaborativa, autodidacta y con muchas ganas de seguir
+        aprendiendo y creciendo en este mundo IT que tanto me gusta.
       </p>
     </section>
   );
