a15971225272bbf5a28a3a2a852f45f507369223
camila.pereyra <campereyra23051995@gmail.com>
2024-03-26 23:12:12 -0300

    Correcciones ortográficas

--- src/components/ContainerAboutMe/ContanerAboutMe.jsx
+++ src/components/ContainerAboutMe/ContanerAboutMe.jsx
@@ -7,20 +7,19 @@ const ContanerAboutMe = () => {
       <p className="aboutMeParagraph">
         Actualmente me defino como una apasionada por la tecnología. Este mundo
         lo descrubí en el 2020 cuando nos encontrabamos frente a la gran
-        incertidumbre que fue la pandemia...La virtualidad que vino de su mano y
-        mi espiritu curioso hicieron que me avalance y quiera explorar nuevos
-        horizontes. Comencé a cursar en ese entonces la carrera Licenciatura en
+        incertidumbre que fue la pandemia... La virtualidad que vino de su mano
+        y mi espíritu curioso hicieron que me avalance y quiera explorar nuevos
+        horizontes. Comencé a cursar entonces la carrera Licenciatura en
         Sistemas en la UNLa. Al comenzar la universidad reafirme que este mundo
-        era para mí, qe quería dedicarme a esto.
+        era para mí, que quería dedicarme a esto.
       </p>
       <p className="aboutMeParagraph">
         Durante el año 2022, emprendí el desafío de realizar varios cursos,
         algunos relacionados al desarrollo frontend: Desarrollo Web, Javascript
         y React JS y uno relacionado al testing: Tester Funcional Manual.
         Gracias a los mismos, hoy soy Frontend Developer ReactJS y Tester QA
-        Funcional Manual. Mi idea es seguir capacitandome, no solo con cursos
-        sino retomar la carrera que deje en pausa por cuestiones de tiempos. Mi
-        gran sueño es poder trabajar en el mundo IT.
+        Funcional Manual. Mi idea es seguir capacitándome, no sólo con cursos
+        sino retomar la carrera que dejé en pausa por cuestiones de tiempos.
       </p>
       <p className="aboutMeParagraph">
         Respecto al ámbito laboral, tengo experiencia freelance desarrollando
